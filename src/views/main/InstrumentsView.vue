cf2ee25486f963e5029cb522c7b1f9fa9887a7d0
MWinterE <mwexcellency@outlook.com>
2022-10-31 15:04:29 +0300

    v1.0.0

--- src/views/main/InstrumentsView.vue
+++ src/views/main/InstrumentsView.vue
@@ -1,6 +1,25 @@
 <template>
     <main class="main">
-        <img v-if="showBannerInstruments === 'true'" src="@/assets/images/main/image.jpg" alt="" />
+        <img
+            v-if="showBannerInstruments === 'true'"
+            src="@/assets/images/main/construction-instruments.jpg"
+            alt=""
+        />
+        <h1>Instruments Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+            voluptates et eligendi molestiae aliquam ipsum quos neque corporis,
+            possimus sapiente velit accusamus vero quo cumque nesciunt! Ab
+            voluptas consequuntur minima, dignissimos impedit in molestiae
+            tenetur? Neque expedita delectus hic nulla quam. Placeat dignissimos
+            ducimus recusandae corrupti consequatur quaerat at magni, vitae
+            cupiditate maiores quo harum facere. Reprehenderit voluptas et
+            nostrum ut fugit modi a ratione quibusdam, repellendus nam,
+            cupiditate culpa est quo eum neque in. Iure laboriosam error totam
+            libero. Architecto, consequuntur et, voluptates odio inventore
+            possimus voluptatum, maiores quas officia saepe sit optio nostrum
+            unde repudiandae aliquid facere eveniet!
+        </p>
     </main>
 </template>
 
@@ -10,12 +29,12 @@ import { getCookie, deleteCookie } from "@/use/cookie.js";
 export default {
     data() {
         return {
-            showBannerInstruments: '',
+            showBannerInstruments: "",
         };
     },
     mounted() {
         if (getCookie("showBannerInstruments") === "true") {
-            this.showBannerInstruments = 'true';
+            this.showBannerInstruments = "true";
         } else if (getCookie("showBannerInstruments") != "true") {
             document.cookie =
                 "showBannerInstruments=true; path=/; expires=Fri, 1 Jan 2038 00:00:00 GMT";
@@ -29,7 +48,21 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.main
+    align-items: center
+
 img
+    margin-bottom: 128px
+
     width: 100%
     height: 512px
+
+h1
+    margin-bottom: 64px
+
+    font-size: 64px
+
+p
+    padding-left: 64px
+    padding-right: 64px
 </style>
--- src/views/main/MusicView.vue
+++ src/views/main/MusicView.vue
@@ -2,9 +2,11 @@
     <main class="main">
         <img
             v-if="showBannerMusic === 'true'"
-            src="@/assets/images/main/image.jpg"
+            src="@/assets/images/main/music-instruments.jpg"
             alt=""
         />
+        <h1>Music Page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex repellendus eveniet sequi, ab quibusdam debitis aliquid consectetur dolorem officia amet, sed maxime error ducimus est totam quasi suscipit quod, blanditiis adipisci laboriosam magni. Optio hic excepturi porro qui! Optio obcaecati ab odit qui enim odio, magnam sed aliquam nisi delectus in quae doloribus repudiandae repellendus laudantium recusandae rerum perferendis blanditiis inventore facere laborum vel, voluptatibus dolores! Nihil quibusdam porro vero, at fugiat adipisci doloremque voluptatibus dolor aperiam a quisquam. Nostrum omnis eum voluptatum illo soluta necessitatibus laboriosam exercitationem! Dolore consequuntur distinctio eveniet, earum corporis dolor temporibus suscipit tempore. Ut, corrupti!</p>
     </main>
 </template>
 
@@ -33,7 +35,21 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.main
+    align-items: center
+
 img
+    margin-bottom: 128px
+
     width: 100%
     height: 512px
+
+h1
+    margin-bottom: 64px
+
+    font-size: 64px
+
+p
+    padding-left: 64px
+    padding-right: 64px
 </style>
